de4ed13ed282b99dd83ea6564235b596f9b92bcb
Sylvia <sylvia@Sylvias-MacBook-Pro.local>
2017-01-11 15:05:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toaster::Application.config.secret_key_base = '7aff0ba0df63b7c8bc45bf6b230472f14e6cd542a54af5d882fcf06edeb4818c5da81e3fa8a302e62aab58688335f85a6bac706700f8cb01d712db504c4e11e2'
